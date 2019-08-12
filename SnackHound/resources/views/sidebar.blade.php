b4a1247039564b901b9dcd834a3e0ca38d05d311
Joao Felicio <joaolfelicio@gmail.com>
2019-08-12 16:00:39 +0200

    Footer html and test route

---
+++ SnackHound/resources/views/sidebar.blade.php
@@ -0,0 +1,19 @@
+<div class="sidebar">
+    <div class="sidebar-logo">
+        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png" alt="Logo of SnackHound."></a>
+    </div>
+
+    <div class="sidebar-divisor"></div>
+
+    <div class='sidebar-items'>
+        <ul>
+            <li><a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Coverage area </li>
+            <li><a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Lunchbar </li>
+            <li><a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> About Us</li>
+            <li><a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Food Truck Owner</li>
+            <li><a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Login | Sign Up</li>
+            <li><a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Account Settings </li>
+        </ul>
+    </div>
+
+</div>
