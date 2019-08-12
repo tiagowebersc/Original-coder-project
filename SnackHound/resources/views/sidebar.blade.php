4573ce7a2aea91067443d3689c27ac8b016eff17
Joao Felicio <joaolfelicio@gmail.com>
2019-08-12 16:58:04 +0200

    Sidebar template

--- SnackHound/resources/views/sidebar.blade.php
+++ SnackHound/resources/views/sidebar.blade.php
@@ -1,19 +1,18 @@
+<link rel="stylesheet" href="{{ URL::asset('css/sidebar.css') }}" />
+
 <div class="sidebar">
     <div class="sidebar-logo">
-        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png" alt="Logo of SnackHound."></a>
+        <a href="#"><img class='sidebar-logo-img' src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png" alt="Logo of SnackHound."></a>
     </div>
-
-    <div class="sidebar-divisor"></div>
 
     <div class='sidebar-items'>
         <ul>
-            <li><a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Coverage area </li>
-            <li><a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Lunchbar </li>
-            <li><a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> About Us</li>
-            <li><a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Food Truck Owner</li>
-            <li><a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Login | Sign Up</li>
-            <li><a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Account Settings </li>
+            <li><a href="#"> <img class='sidebar-items-img' src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Coverage area </a></li>
+            <li><a href="#"> <img class='sidebar-items-img' src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Lunchbar </a></li>
+            <li><a href="#"> <img class='sidebar-items-img' src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> About Us </a></li>
+            <li><a href="#"> <img class='sidebar-items-img' src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Food Truck Owner </a></li>
+            <li><a href="#"> <img class='sidebar-items-img' src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Login | Sign Up </a></li>
+            <li><a href="#"> <img class='sidebar-items-img' src="https://upload.wikimedia.org/wikipedia/commons/f/fc/UPC_logo_transparente.png"> Account Settings </a></li>
         </ul>
     </div>
-
 </div>
