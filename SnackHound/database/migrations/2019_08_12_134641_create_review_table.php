f9392f52bfdad6e85c0c91a2e0a122813a6d260f
Tiago <tiagowebersc@gmail.com>
2019-08-12 16:41:47 +0200

    User table migration/model/seed

---
+++ SnackHound/database/migrations/2019_08_12_134641_create_review_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('review', function (Blueprint $table) {
+            $table->bigIncrements('id_review');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('review');
+    }
+}
