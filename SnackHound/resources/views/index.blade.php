35a803dbc706306261d285409ff4ab82d981ce41
nasser ninja <alkar0abdulnasser@gmail.com>
2019-08-14 17:36:46 +0200

    working with the map

--- SnackHound/resources/views/index.blade.php
+++ SnackHound/resources/views/index.blade.php
@@ -28,8 +28,18 @@
     </section>
 </header>
 <main>
-
-    <h1></h1>
+        <div id="map"></div>
+        <script>
+          var map;
+          function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+              center: {lat:  34.397, lng: 150.644},
+              zoom: 8
+            });
+          }
+        </script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtlX1KloHpjKujAEto6qDggr_-ibVatcA&callback=initMap"
+        async defer></script>
 </main>
 
 @endsection
