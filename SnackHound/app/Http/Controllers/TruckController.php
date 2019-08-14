422a93eaf2ad1cce5d9905f0de4b55cca56acb53
Joao Felicio <joaolfelicio@gmail.com>
2019-08-14 14:51:31 +0200

    small update dashboard

--- SnackHound/app/Http/Controllers/TruckController.php
+++ SnackHound/app/Http/Controllers/TruckController.php
@@ -5,17 +5,14 @@ namespace App\Http\Controllers;
 use App\Order;
 use App\Truck;
 use App\View_order;
-use DB;
 use Illuminate\Http\Request;
 
 class TruckController extends Controller{
 
     public function getOrders($id) {
-        $orders = View_order::all()->where("id_truck", $id);
         $truck = Truck::find($id);
+        $orders = View_order::all()->where("id_truck", $id);
 
         return view('truckOwnerDashboard', ['orders' => $orders, 'truck' => $truck]);
     }
-
-
 }
