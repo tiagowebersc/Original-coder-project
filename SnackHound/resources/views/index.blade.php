6c4c69ac0ae5efd32100d826e2e7b5c717835692
nasser ninja <alkar0abdulnasser@gmail.com>
2019-08-14 11:53:43 +0200

    working in the index

--- SnackHound/resources/views/index.blade.php
+++ SnackHound/resources/views/index.blade.php
@@ -1,12 +1,26 @@
-<link rel="stylesheet" href="{{ URL::asset('/css/index.css') }}" />
-
 @extends('layouts.customerSidebar')
 
-
+@section('css')
+<link rel="stylesheet" href="{{ URL::asset('/css/index.css') }}" />
+@endsection
 
 @section('title', 'SnackHound')
 
 @section('content')
 
+ <header class="bg-image">
+    <section class="bg-text">
+        <h1>Hungry?</h1>
+        <p>SnackHound sniffs out the nearest food trucks in your are and lets you order, pay,
+            and get directions to your favorite stop. Enter in your address below to get started!</p>
+
+        <form action="">
+            <input type="search" class="search" placeholder="Enter your address..." name="search-location">
+            <input type="submit" class="submit-location" name="submit-location" value="Search">
+            <p>or</p>
+            <input type="submit" value="Turn on Location" name="turn-on-locaton" class="turn-on-location" >
+        </form>
+     </section>
+</header>
 
 @endsection
