ca5deaffbd5331797dd08d2614504fc359129912
Joao Felicio <joaolfelicio@gmail.com>
2019-08-15 21:31:35 +0200

    Food tuck connection with session and working on filter orders

--- SnackHound/app/Http/Controllers/TruckController.php
+++ SnackHound/app/Http/Controllers/TruckController.php
@@ -5,29 +5,63 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Models\Truck;
 use App\Models\View_order;
-use DB;
+use Session;
 use Illuminate\Http\Request;
 
 class TruckController extends Controller
 {
 
-    public function getOrders()
-    {
-        // $orders = View_order::all()->where("id_truck", 1);
-        $truck = Truck::find(1);
-        $orders = View_order::where("id_truck", 1)->orderBy('created_at', 'desc')->get();
-        $ordersCount = View_order::where("id_truck", 1)->count();
+    public function getOrders(){
+        // Check if user is food truck owner
+        if (Session::get('user_type') === 1) {
+            $userId = Session::get('id_user');
 
-        return view('truckOwnerDashboard', ['orders' => $orders, 'truck' => $truck, 'ordersCount' => $ordersCount]);
+            $truck = Truck::where('id_user', $userId)->first();
+            $orders = View_order::where("id_truck", $truck->id_truck)->orderBy('created_at', 'desc')->get();
+            $ordersCount = View_order::where("id_truck", $truck->id_truck)->count();
+
+            return view('truckOwnerDashboard', ['orders' => $orders, 'truck' => $truck, 'ordersCount' => $ordersCount]);
+         } else {
+             // If user is not a truck owner, return to index
+             return redirect()->route('index');
+         }
     }
 
     public function filterOrder(Request $request) {
-        // CREATE A NEW VIEW
-        // NOR WORKING
-        $truck = Truck::find(1);
-        $orders = View_order::where("id_truck", 1)->where('created_at', [$request->fromDate, $request->toDate])->get();
-        $ordersCount = View_order::where("id_truck", 1)->where('created_at', [$request->fromDate, $request->toDate])->count();
+        // CREATE A NEW VIEW (USER, ORDER, ORDER CONTENT)
+        // NOT WORKING
 
-        return view('truckOwnerDashboard', ['orders' => $orders, 'truck' => $truck, 'ordersCount' => $ordersCount]);
+        if (Session::get('user_type') === 1) {
+
+            $userId = Session::get('id_user');
+
+            $truck = Truck::where('id_user', $userId)->first();
+
+            if($request->typeSearch === "user") {
+                // Search orders by users
+            } else if($request->typeSearch === "date") {
+                // Search orders by date
+                $orders = View_order::where("id_truck", $userId)->whereBetween('created_at', [$request->fromDate, $request->toDate])->get();
+                $ordersCount = View_order::where("id_truck", $userId)->whereBetween('created_at', [$request->fromDate, $request->toDate])->count();
+
+            } else {
+                // Search orders by amount
+                $orders = View_order::where("id_truck", $userId)->whereBetween('total', [$request->fromAmount, $request->toAmount])->get();
+                $ordersCount = View_order::where("id_truck", $userId)->whereBetween('total', [$request->fromAmount, $request->toAmount])->count();
+            }
+
+            // If user inserted info, return filtered, if not return all
+            if(isset($orders) && isset($ordersCount)) {
+                return view('truckOwnerDashboard', ['orders' => $orders, 'truck' => $truck, 'ordersCount' => $ordersCount]);
+            } else {
+                return $this->getOrders();
+            }
+
+
+         } else {
+             // If user is not a truck owner, return to index
+             return redirect()->route('index');
+         }
+
     }
 }
