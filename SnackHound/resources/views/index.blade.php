efc1d5c4621e6d9bda89af2397a52b1f029a8427
nasser ninja <alkar0abdulnasser@gmail.com>
2019-08-16 14:50:07 +0200

    almaost done with the cards/index

--- SnackHound/resources/views/index.blade.php
+++ SnackHound/resources/views/index.blade.php
@@ -3,6 +3,10 @@
 @section('css')
 <link rel="stylesheet" href="{{ URL::asset('/css/index.css') }}" />
 <link href="https://fonts.googleapis.com/css?family=Raleway:400,700|Roboto+Slab&display=swap" rel="stylesheet">
+<link
+href="https://fonts.googleapis.com/icon?family=Material+Icons"
+rel="stylesheet"
+/>
 @endsection
 
 @section('title', 'SnackHound - Homepage')
@@ -31,9 +35,79 @@
 <main>
     <section class="main-container">
         <section class="cards-container">
-            <h1>dmfsdfbn</h1>
+            <?php
+            foreach ($trucks as $truck) {
+                # code...
+                ?>
+                <div class="wrapper">
+                        <div class="container">
+                          <div class="top">
+                            <img
+                              src="{{URL::asset('assets/IMGS/Food Trucks/'. $truck['image'])}}"
+                              alt=""
+                            />
+                          </div>
+                          <div class="bottom">
+                            <div class="left">
+                              <div class="details">
+                              <h1><?= $truck['name'] ?></h1>
+                                <p>starts</p>
+                                <p>
+                                  <i class="material-icons">
+                                    place </i
+                                  >250
+                                </p>
+                                <hr/>
+                                <h1>Chair</h1>
+                                <p>£250</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="inside">
+                          <div class="icon"><i class="material-icons">info_outline</i></div>
+                          <div class="contents">
+                            <table>
+                              <tr>
+                                <th>Width</th>
+                                <th>Height</th>
+                              </tr>
+                              <tr>
+                                <td>3000mm</td>
+                                <td>4000mm</td>
+                              </tr>
+                              <tr>
+                                <th>Something</th>
+                                <th>Something</th>
+                              </tr>
+                              <tr>
+                                <td>200mm</td>
+                                <td>200mm</td>
+                              </tr>
+                              <tr>
+                                <th>Something</th>
+                                <th>Something</th>
+                              </tr>
+                              <tr>
+                                <td>200mm</td>
+                                <td>200mm</td>
+                              </tr>
+                              <tr>
+                                <th>Something</th>
+                                <th>Something</th>
+                              </tr>
+                              <tr>
+                                <td>200mm</td>
+                                <td>200mm</td>
+                              </tr>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
+                      <?php
+            }
+            ?>
         </section>
-
         <section class="map-container">
             <div id="map"></div>
         </section>
@@ -88,9 +162,10 @@
                         },
                         {
                         coords:{lat:49.495640, lng: 5.985173},
-                        iconImage:'<img src="{{URL::asset('assets/IMGS/Food Trucks/mr.froopy-frozenjoghurt.jpg')}}">',
+                        content:'<img src="{{URL::asset('assets/IMGS/Food Trucks/mr.froopy-frozenjoghurt.jpg')}}">',
                         iconImage:'https://img.icons8.com/officel/40/000000/marker.png'
                         }
+
                     ];
 
                     // Loop through markers
--- SnackHound/routes/web.php
+++ SnackHound/routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-})->name('index');;
+Route::get('/','indexController@index')->name('index');;
 
 Route::get('/login', 'UserController@login');
 
