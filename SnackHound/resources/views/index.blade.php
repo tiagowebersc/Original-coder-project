7299c020c81a4626f23827c7dc66428422ac9f5a
Tiago Weber <tiagowebersc@gmail.com>
2019-08-17 17:41:00 +0200

    Adapted the pages to the main menu size

--- SnackHound/resources/views/index.blade.php
+++ SnackHound/resources/views/index.blade.php
@@ -12,7 +12,7 @@ rel="stylesheet"
 @section('title', 'SnackHound - Homepage')
 
 @section('content')
-
+<div class="background">
  <header class="bg-image">
     <section class="bg-text">
         <h1>Hungry?</h1>
@@ -123,6 +123,7 @@ rel="stylesheet"
         </section>
     </section>
 </main>
+</div>
             <script>
                 function initMap(){
                     // Map options
