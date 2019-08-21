74d49ac2ef24b2fedf4ba5117cdc632bdff24222
DiedonMxh <maxhuni.diedon@gmail.com>
2019-08-21 16:51:02 +0200

    edited Review box
    Co-Authored-By: mxhdiedon <mxhdiedon@users.noreply.github.com>

--- SnackHound/resources/views/scrollBox.blade.php
+++ SnackHound/resources/views/scrollBox.blade.php
@@ -3,22 +3,41 @@
 <link rel="stylesheet" href="{{URL::asset('/css/scrollbox.css')}}" />
 
 
-<section>
-    <h1>REVIEWS:</h1>
-        
-    <div class="scrollBoxReview">
-        <div class="userInfo">
-            <p class="userNameInBox">- User Name</p><p>Rating</p><p>(Date):</p>
-        </div>
+<section class="mainReview">
     
-        <div class="userReview">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Aliquid tempora magni placeat labore pariatur, laboriosam optio molestiae 
-            voluptatibus accusamus cupiditate impedit corrupti consequatur repellendus ea ipsam
-            libero! Esse, molestiae neque!
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+    <div class="reviewBox">
+        
+        <div class="scrollBoxReview">
+                <h1>REVIEWS:</h1>
+                <div class="formReview">
+
+                    <div class="userInfo">
+                        <p class="userNameInBox">User Name</p><p>Rating</p><p>(Date):</p>
+                    </div>
+                    
+                    <div class="userReview">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Aliquid tempora magni placeat labore pariatur, laboriosam optio molestiae 
+                        voluptatibus accusamus cupiditate impedit corrupti consequatur repellendus ea ipsam
+                    </div>
+                </div>
         </div>
-         
+
+        <hr class="separator">
+
+        <div class="leavReview">
+            <h1>Leave A Review</h1>
+            <form action="" method="POST">
+                <div>
+                    <h3>Comments:</h3>
+                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                    <p>Star</p>
+                </div>
+                <input type="submit" value="Post">
+            </form>
+
+
+        </div>
+        
     </div>
-            
 </section>
