db2f000ee0349dce2ece356fb2a8127bf06e2be4
DiedonMxh <maxhuni.diedon@gmail.com>
2019-08-22 15:54:55 +0200

    edited review part
    Co-Authored-By: mxhdiedon <mxhdiedon@users.noreply.github.com>

--- SnackHound/resources/views/scrollBox.blade.php
+++ SnackHound/resources/views/scrollBox.blade.php
@@ -1,6 +1,10 @@
 
 
 <link rel="stylesheet" href="{{URL::asset('/css/scrollbox.css')}}" />
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,600&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400&display=swap" rel="stylesheet">
+
 
 
 <section class="mainReview">
@@ -8,11 +12,14 @@
     <div class="reviewBox">
         
         <div class="scrollBoxReview">
-                <h1>REVIEWS:</h1>
+            
+            <h1>REVIEWS:</h1>
+            <div class="reviewList">
+                
                 <div class="formReview">
-
+                    
                     <div class="userInfo">
-                        <p class="userNameInBox">User Name</p><p>Rating</p><p>(Date):</p>
+                        <p class="userNameInBox">User Name</p><p>Rating</p><p class="dateReview">(Date):</p>
                     </div>
                     
                     <div class="userReview">
@@ -21,6 +28,35 @@
                         voluptatibus accusamus cupiditate impedit corrupti consequatur repellendus ea ipsam
                     </div>
                 </div>
+
+                <div class="formReview">
+                    
+                        <div class="userInfo">
+                            <p class="userNameInBox">User Name</p><p>Rating</p><p class="dateReview">(Date):</p>
+                        </div>
+                        
+                        <div class="userReview">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Aliquid tempora magni placeat labore pariatur, laboriosam optio molestiae 
+                            voluptatibus accusamus cupiditate impedit corrupti consequatur repellendus ea ipsam
+                        </div>
+                    </div>
+
+                    <div class="formReview">
+                    
+                            <div class="userInfo">
+                                <p class="userNameInBox">User Name</p><p>Rating</p><p class="dateReview">(Date):</p>
+                            </div>
+                            
+                            <div class="userReview">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Aliquid tempora magni placeat labore pariatur, laboriosam optio molestiae 
+                                voluptatibus accusamus cupiditate impedit corrupti consequatur repellendus ea ipsam
+                            </div>
+                        </div>
+
+
+            </div>
         </div>
 
         <hr class="separator">
@@ -30,10 +66,14 @@
             <form action="" method="POST">
                 <div>
                     <h3>Comments:</h3>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
-                    <p>Star</p>
+                    <div class="ratingReview">
+                        <textarea name="" id="" cols="30" rows="10"></textarea>
+                        <br>
+                        <img class="starlogo" src="{{URL::asset('assets/ICONS/Food Truck Cards/icons8-star-blank.svg')}}" alt="">
+                        <br>
+                        <input type="submit" value="Post">
+                    </div>
                 </div>
-                <input type="submit" value="Post">
             </form>
 
 
