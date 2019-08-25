2351268030297b0152f959eed014e49d8e5849c4
Bobby Spencer <bbdgtl@gmail.com>
2019-08-25 19:24:28 +0200

    Add a 404 page

---
+++ SnackHound/resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404 ERROR</title>
+
+</head>
+<body style="background-color: #333333">
+<div class="container">
+    <div class="row">
+    <img class="404" src="{{URL::asset('assets/IMGS/404/404.svg')}}" alt="a broken down food truck">
+    </div>
+</div>
+</body>
+</html>
+
+
